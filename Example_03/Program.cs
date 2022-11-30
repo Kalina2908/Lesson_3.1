342cd93c91779e63cc9ea565992b38f69050c2fe
Maria Kalina <kalina2908@mail.ru>
2022-11-30 23:34:34 +0300

    add example 03 for Lesson 3.1

---
+++ Example_03/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine() ??"";
+if (username.ToLower() == "maria")
+{
+    Console.WriteLine("Ура, это Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
